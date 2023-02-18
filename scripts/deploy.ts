29129f36b9550830604304e099c76f3fbf39c27f
VTAkbay <vahidturabakbay1@gmail.com>
2023-02-19 01:02:01 +0300

    feat: update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,19 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  function generateRandomUint16(): number {
+    return Math.floor(Math.random() * 2 ** 16);
+  }
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const nftCount = 50;
+  const seed = generateRandomUint16();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const Nft = await ethers.getContractFactory("Nft");
+  const nft = await Nft.deploy(nftCount, seed);
 
-  await lock.deployed();
+  await nft.deployed();
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(`Nft deployed to ${nft.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
